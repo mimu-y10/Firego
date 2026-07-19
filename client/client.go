3b03cbb9a55e74a6882fa72d48f2757cf18c5f66
mimuyuto <mimu.yuto@icloud.com>
2026-07-20 00:55:45 +0900

    docs: add doc comments to client package

--- client/client.go
+++ client/client.go
@@ -3,10 +3,12 @@ package client
 
 import "cloud.google.com/go/firestore"
 
+// Client wraps the Google Cloud Firestore client used by Firego.
 type Client struct {
 	firestore *firestore.Client
 }
 
+// New creates a Firego client backed by client.
 func New(client *firestore.Client) *Client {
 	return &Client{
 		firestore: client,
